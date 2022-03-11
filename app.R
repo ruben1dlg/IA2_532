57bb095b5e0724cd4359289ace7c995fd6a9976d
ruben1dlg <ruben1dlg@gmail.com>
2022-03-10 18:22:25 -0800

    First commit

---
+++ app.R
@@ -0,0 +1,41 @@
+library(dash)
+library(dashBootstrapComponents)
+library(dashCoreComponents)
+library(ggplot2)
+library(plotly)
+library(purrr)
+
+app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
+
+df <- readr::read_csv(here::here('data', 'olympics_data.csv'))
+
+app$layout(
+    dbcContainer(
+        list(
+            dccGraph(id='plot-area'),
+            dccDropdown(
+                id='sport-select',
+                options = as.list(unique(df$sport)) %>%
+                    purrr::map(function(col) list(label = col, value = col)), 
+                value='Judo')
+        )
+    )
+)
+
+app$callback(
+    output('plot-area', 'figure'),
+    list(input('sport-select', 'value')),
+    function(sport_sel) {
+        df_filtered <- df %>%
+            filter(sport == sport_sel)
+        p <- ggplot(df_filtered, aes(x = height)) +
+            geom_histogram() +
+            scale_x_log10() +
+            ggthemes::scale_color_tableau()
+        ggplotly(p)
+    }
+)
+
+app$run_server(debug = T)
+
+app$run_server(host = '0.0.0.0')
